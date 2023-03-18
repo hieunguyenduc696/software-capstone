ee62303ffe8428bc79bdcdf434b523f3ede3d12a
Nhat Dang <nhatdang2604@gmail.com>
2023-03-18 17:25:15 +0700

    solution-sketch-be: Temporaly finish solution sketch

--- software-capstone-be/src/app.ts
+++ software-capstone-be/src/app.ts
@@ -1,7 +1,7 @@
 "use strict"
 
 import express, {Express} from 'express';
-import { APP_PORT } from './app/constants/configConstants';
+import { APP_PORT } from './app/constants/config_constants';
 
 const app: Express = express();
 const port: string | undefined = APP_PORT;    //default port is 8088
@@ -10,6 +10,7 @@ const port: string | undefined = APP_PORT;    //default port is 8088
 app.use(express.json()); 
 
 //Routes registrations
+
 
 app.listen(port, () => {
     console.log(`[Server]: Server is serving at port: ${port}`);
