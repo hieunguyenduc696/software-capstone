75a21798a32a6d4369d0f93476a55395f28db02c
khduyentr <khduyentr2212@gmail.com>
2023-03-29 14:16:35 +0700

    add section one component

--- software-capstone-fe/src/pages/Create/AddTest.tsx
+++ software-capstone-fe/src/pages/Create/AddTest.tsx
@@ -1,78 +1,12 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./AddTest.module.css";
 import { Row, Col, Tabs, Typography } from "antd";
 import { AppHeader } from "../../components/AppHeader";
 import TrueFalseType from "components/QuestionType/MultipleChoice/TrueFalseType";
 import TrueFalseInstruction from "components/Instruction/TrueFalseInstruction/TrueFalseInstruction";
-import { EditOutlined } from "@ant-design/icons";
 
 import type { TabsProps } from "antd";
-import { Divider } from "antd";
-import { InboxOutlined } from "@ant-design/icons";
-
-const SectionOne = () => {
-  const [title, setTitle] = useState<string>("Paragraph Title");
-  const [content, setContent] = useState<string>("Enter paragraph content....");
-  const [image, setImage] = useState<any>();
-  const inputFileRef = useRef<HTMLInputElement>(null);
-
-  const handleAddFileClick = () => {
-    if (inputFileRef.current) {
-      inputFileRef.current.click();
-    }
-  };
-
-  return (
-    <>
-      <Row>
-        <Col span={24} style={{ display: "flex", justifyContent: "center" }}>
-          <div
-            style={{
-              width: "90%",
-              height: "150px",
-              borderRadius: "10px",
-              border: "1px solid #4985CD",
-              display: "flex",
-              flexDirection: "column",
-              alignContent: "center",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <InboxOutlined style={{ fontSize: "45px", color: "#4985CD" }} />
-            <p style={{ fontSize: "20px" }}>Click to upload Image</p>
-          </div>
-        </Col>
-      </Row>
-
-      <Divider style={{ color: "#000" }} />
-      <Typography.Title
-        className={`${styles.title}`}
-        editable={{
-          icon: <EditOutlined />,
-          tooltip: "Edit title",
-          onChange: setTitle,
-          enterIcon: null,
-        }}
-        level={3}
-        style={{ margin: 0 }}
-      >
-        {title}
-      </Typography.Title>
-      <Typography.Paragraph
-        editable={{
-          icon: <EditOutlined />,
-          tooltip: "click to edit text",
-          onChange: setContent,
-          enterIcon: null,
-        }}
-        className={`${styles.paragraph} ${styles.full}`}
-      >
-        {content}
-      </Typography.Paragraph>
-    </>
-  );
-};
+import SectionOne from "components/Section/SectionOne/SectionOne";
 
 const items: TabsProps["items"] = [
   {
