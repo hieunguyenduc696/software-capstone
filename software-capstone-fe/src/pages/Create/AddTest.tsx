49824cbab55b245143e6fce3f4ebc9eec62b1f8d
khduyentr <khduyentr2212@gmail.com>
2023-04-04 20:48:58 +0700

    [Software-52] finish component

--- software-capstone-fe/src/pages/Create/AddTest.tsx
+++ software-capstone-fe/src/pages/Create/AddTest.tsx
@@ -3,10 +3,12 @@ import styles from "./AddTest.module.css";
 import { Row, Col, Tabs, Typography } from "antd";
 import { AppHeader } from "../../components/AppHeader";
 import TrueFalseType from "components/QuestionType/MultipleChoice/TrueFalseType";
+import ShortAnswerType from "components/QuestionType/ShortAnswer/ShortAnswerType";
 import TrueFalseInstruction from "components/Instruction/TrueFalseInstruction/TrueFalseInstruction";
 
 import type { TabsProps } from "antd";
 import SectionOne from "components/Section/SectionOne/SectionOne";
+
 
 const items: TabsProps["items"] = [
   {
@@ -31,6 +33,23 @@ const items: TabsProps["items"] = [
 const AddingTestPage = () => {
   const onChange = (key: string) => {
     console.log(key);
+  };
+
+  const [answer, setAnswer] = useState<string>();
+  const [question, setQuestion] = useState<string>();
+  const answerRef = useRef<HTMLInputElement>(null);
+  const questionRef = useRef<HTMLInputElement>(null);
+
+  const handleAnswerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (answerRef.current) {
+      setAnswer(event.target.value);
+    }
+  };
+
+  const handleQuestionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (questionRef.current) {
+      setQuestion(event.target.value);
+    }
   };
 
   return (
@@ -62,22 +81,70 @@ const AddingTestPage = () => {
           </Row>
 
           <Row>
-            <TrueFalseInstruction from={1} to={7} />
+            {/* <TrueFalseInstruction from={1} to={7} />
             <TrueFalseType order={1} />
             <TrueFalseType order={2} />
             <TrueFalseType order={3} />
             <TrueFalseType order={4} />
             <TrueFalseType order={5} />
             <TrueFalseType order={6} />
+            <TrueFalseType order={7} /> */}
             <TrueFalseType order={7} />
 
-            <Col span={24} style={{ boxSizing: "border-box" }} className={`${styles.shortAnswer}`}>
-              <Typography.Paragraph
-                editable
-              >
-                {'Enter the question'}
-              </Typography.Paragraph>
-            </Col>
+            <ShortAnswerType order={7}/>
+            {/* <Col
+              span={24}
+              style={{ boxSizing: "border-box" }}
+              className={`${styles.shortAnswer}`}
+            >
+              <Row>
+                <Col span={24}>
+                  <input
+                    type="text"
+                    required
+                    ref={questionRef}
+                    style={{
+                      fontSize: "16px",
+                      width: "100%",
+                      backgroundColor: "transparent",
+                      marginBottom: "10px"
+                    }}
+                    className={styles["ip-title"]}
+                    value={question}
+                    onChange={handleQuestionChange}
+                    placeholder="Enter the question"
+                  />
+                </Col>
+              </Row>
+
+              <Row>
+                <Col
+                  span={14}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                  }}
+                >
+                  <div className={styles.number}>{`1`}</div>
+                  <input
+                    type="text"
+                    required
+                    ref={answerRef}
+                    style={{
+                      fontSize: "16px",
+                      width: "100%",
+                      backgroundColor: "transparent",
+                    }}
+                    className={styles["ip-title"]}
+                    value={answer}
+                    onChange={handleAnswerChange}
+                    placeholder="Enter the answer for this question"
+                  />
+                </Col>
+              </Row>
+            </Col> */}
           </Row>
         </Col>
       </Row>
--- software-capstone-fe/src/pages/PostTest/PostTest.tsx
+++ software-capstone-fe/src/pages/PostTest/PostTest.tsx
@@ -1,157 +1,212 @@
-import { Button, Col, Image, Row } from "antd"
-import { AppHeader, UploadImage } from "components"
-import { PreviewTestItem } from "components/PreviewTestItem"
-import { useRef, useState } from "react"
-import styles from "./PostTest.module.css"
-import { useNavigate } from "react-router"
+import { Button, Col, Image, Row } from "antd";
+import { AppHeader, UploadImage } from "components";
+import { PreviewTestItem } from "components/PreviewTestItem";
+import { useRef, useState } from "react";
+import styles from "./PostTest.module.css";
+import { useNavigate } from "react-router";
 
 const mockPreviewTestItems = [
-    {
-        image: 'default.png',
-        title: 'IELTS Mock test 2023',
-        publishDate: '28/03/2023'
-    },
-    {
-        image: 'default.png',
-        title: 'IELTS Mock test 2022',
-        publishDate: '28/03/2022'
-    },
-    {
-        image: 'default.png',
-        title: 'IELTS Mock test 2021',
-        publishDate: '28/03/2021'
-    }
-]
+  {
+    image: "default.png",
+    title: "IELTS Mock test 2023",
+    publishDate: "28/03/2023",
+  },
+  {
+    image: "default.png",
+    title: "IELTS Mock test 2022",
+    publishDate: "28/03/2022",
+  },
+  {
+    image: "default.png",
+    title: "IELTS Mock test 2021",
+    publishDate: "28/03/2021",
+  },
+];
 
 export const PostTest = () => {
-    const titleRef = useRef(null);
-    const [title, setTitle] = useState<string>("IELTS Recent mock test");
-    const [err, setErr] = useState<any>({});
-    const navigate = useNavigate();
+  const titleRef = useRef(null);
+  const [title, setTitle] = useState<string>("IELTS Recent mock test");
+  const [err, setErr] = useState<any>({});
+  const navigate = useNavigate();
 
-    const handleTitleChange = (e: any) => {
-        const len = e.target.value.length;
-        if (len <= 40) {
-            setTitle(e.target.value);
-            setErr({ ...err, title: "" });
-        }
-        !len && setErr({ ...err, title: "Title is require" });
-
+  const handleTitleChange = (e: any) => {
+    const len = e.target.value.length;
+    if (len <= 40) {
+      setTitle(e.target.value);
+      setErr({ ...err, title: "" });
     }
+    !len && setErr({ ...err, title: "Title is require" });
+  };
 
-    const handleEditButtonClick = () => {
-        if (titleRef.current) {
-            const ipEle = titleRef.current as HTMLInputElement;
-            ipEle.focus();
-        }
+  const handleEditButtonClick = () => {
+    if (titleRef.current) {
+      const ipEle = titleRef.current as HTMLInputElement;
+      ipEle.focus();
     }
+  };
 
-    const handleAddReadingSectionClick = () => {
-        console.log("add reading test");
-        navigate('/add-test');
-    }
+  const handleAddReadingSectionClick = () => {
+    console.log("add reading test");
+    navigate("/add-test");
+  };
 
-    const handleAddListeningSectionClick = () => {
-        console.log("add listening test");
-    }
+  const handleAddListeningSectionClick = () => {
+    console.log("add listening test");
+  };
 
-    return <div>
-        <AppHeader />
-        <div>
-            <Row style={{ padding: '1.5rem 0 .5rem 1.5rem', backgroundColor: 'white' }}>
-                {/* left */}
-                <Col span={16} >
-                    {/* title */}
-                    <div style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
-                        <div>
-                            <UploadImage />
-                        </div>
-                        <div style={{ flexGrow: 1 }}>
-                            <div style={{ fontSize: '20px', paddingBottom: '.2rem' }}>Title:</div>
-                            <Row style={{ gap: 5 }}>
-                                <Col span={14}>
-                                    <input type="text" required ref={titleRef} style={{ fontSize: '24px', lineHeight: '.8', width: '100%', ...(err.title && { borderBottom: '1px solid black' }), }} className={styles['ip-title']}
-                                        value={title}
-                                        onChange={handleTitleChange}
-                                    />
+  return (
+    <div>
+      <AppHeader />
+      <div>
+        <Row
+          style={{ padding: "1.5rem 0 .5rem 1.5rem", backgroundColor: "white" }}
+        >
+          {/* left */}
+          <Col span={16}>
+            {/* title */}
+            <div style={{ display: "flex", alignItems: "center", gap: 5 }}>
+              <div>
+                <UploadImage />
+              </div>
+              <div style={{ flexGrow: 1 }}>
+                <div style={{ fontSize: "20px", paddingBottom: ".2rem" }}>
+                  Title:
+                </div>
+                <Row style={{ gap: 5 }}>
+                  <Col span={14}>
+                    <input
+                      type="text"
+                      required
+                      ref={titleRef}
+                      style={{
+                        fontSize: "24px",
+                        lineHeight: ".8",
+                        width: "100%",
+                        ...(err.title && { borderBottom: "1px solid black" }),
+                      }}
+                      className={styles["ip-title"]}
+                      value={title}
+                      onChange={handleTitleChange}
+                    />
 
-                                    {<span style={{
-                                        color: 'red',
-                                        height: '1.5rem'
-                                    }}>
-                                        {err.title}
-                                    </span>}
+                    {
+                      <span
+                        style={{
+                          color: "red",
+                          height: "1.5rem",
+                        }}
+                      >
+                        {err.title}
+                      </span>
+                    }
+                  </Col>
+                  <Col span={9}>
+                    <Image
+                      src="edit.png"
+                      style={{
+                        width: "1.5rem",
+                        height: "1.5rem",
+                        cursor: "pointer",
+                      }}
+                      onClick={handleEditButtonClick}
+                      preview={false}
+                    />
+                  </Col>
+                </Row>
+              </div>
+            </div>
 
-                                </Col>
-                                <Col span={9}>
-                                    <Image src='edit.png'
-                                        style={{
-                                            width: '1.5rem',
-                                            height: '1.5rem',
-                                            cursor: 'pointer',
-                                        }}
-                                        onClick={handleEditButtonClick}
-                                        preview={false}
-                                    />
-                                </Col>
-
-                            </Row>
-                        </div>
-                    </div>
-
-                    {/* buttons */}
-                    <Row style={{ padding: '4rem 2rem 0 2rem' }}>
-                        <Col span={12} >
-                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-                                <Image src="read.png" style={{ height: 68 }} preview={false} />
-                                <Button
-                                    style={{
-                                        textTransform: 'uppercase',
-                                        backgroundColor: '#45764B',
-                                        border: 'none',
-                                        color: 'white',
-                                        boxShadow: '4px 4px 4px 0 rgba(0, 0, 0, .25)',
-                                        marginTop: '.5rem'
-                                    }}
-                                    onClick={handleAddReadingSectionClick}
-                                >Add Reading Section</Button>
-                            </div>
-                        </Col>
-                        <Col span={12}>
-                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-                                <Image src="listen.png" style={{ height: 68 }} preview={false} />
-                                <Button
-                                    style={{
-                                        textTransform: 'uppercase',
-                                        backgroundColor: '#5CB1C5',
-                                        border: 'none',
-                                        color: 'white',
-                                        boxShadow: '4px 4px 4px 0 rgba(0, 0, 0, .25)',
-                                        marginTop: '.5rem'
-                                    }}
-                                    onClick={handleAddListeningSectionClick}
-                                >
-                                    Add Listening Section
-                                </Button>
-                            </div>
-                        </Col>
-                    </Row>
-                </Col>
-                {/* right */}
-                <Col span={8} style={{ backgroundColor: '#F2F2F2', padding: '1rem', borderRadius: '10px 0 0 10px' }}>
-                    <div style={{
-                        textTransform: 'uppercase',
-                        color: '#81041F',
-                        fontSize: '20px',
-                        paddingBottom: '.5rem'
-                    }}>
-                        Recent Tests:
-                    </div>
-                    <div>
-                        {mockPreviewTestItems.map((item, i) => <div style={{ paddingTop: '.5rem' }}><PreviewTestItem item={item} /></div>)}
-                    </div>
-                </Col>
+            {/* buttons */}
+            <Row style={{ padding: "4rem 2rem 0 2rem" }}>
+              <Col span={12}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Image
+                    src="read.png"
+                    style={{ height: 68 }}
+                    preview={false}
+                  />
+                  <Button
+                    style={{
+                      textTransform: "uppercase",
+                      backgroundColor: "#45764B",
+                      border: "none",
+                      color: "white",
+                      boxShadow: "4px 4px 4px 0 rgba(0, 0, 0, .25)",
+                      marginTop: ".5rem",
+                    }}
+                    onClick={handleAddReadingSectionClick}
+                  >
+                    Add Reading Section
+                  </Button>
+                </div>
+              </Col>
+              <Col span={12}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Image
+                    src="listen.png"
+                    style={{ height: 68 }}
+                    preview={false}
+                  />
+                  <Button
+                    style={{
+                      textTransform: "uppercase",
+                      backgroundColor: "#5CB1C5",
+                      border: "none",
+                      color: "white",
+                      boxShadow: "4px 4px 4px 0 rgba(0, 0, 0, .25)",
+                      marginTop: ".5rem",
+                    }}
+                    onClick={handleAddListeningSectionClick}
+                  >
+                    Add Listening Section
+                  </Button>
+                </div>
+              </Col>
             </Row>
-        </div>
+          </Col>
+          {/* right */}
+          <Col
+            span={8}
+            style={{
+              backgroundColor: "#F2F2F2",
+              padding: "1rem",
+              borderRadius: "10px 0 0 10px",
+            }}
+          >
+            <div
+              style={{
+                textTransform: "uppercase",
+                color: "#81041F",
+                fontSize: "20px",
+                paddingBottom: ".5rem",
+              }}
+            >
+              Recent Tests:
+            </div>
+            <div>
+              {mockPreviewTestItems.map((item, i) => (
+                <div style={{ paddingTop: ".5rem" }}>
+                  <PreviewTestItem item={item} />
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
-}
+  );
+};
