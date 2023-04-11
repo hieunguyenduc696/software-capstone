6551e66f9ab0e630e44f4f44d560519b10394dc6
dangkl123 <nhatdang2604@gmail.com>
2023-04-12 01:07:31 +0700

    task/create-reading-passage: Finish the task

--- software-capstone-be/src/modules/readingSkill/datatypes/exam.ts
+++ software-capstone-be/src/modules/readingSkill/datatypes/exam.ts
@@ -1,9 +1,19 @@
 type ReadingPassageDao = {
-    wallpaper: string | null,
-    title: string | null,
-    content: string | null,
+    wallpaper: string | null | undefined,
+    title: string | null | undefined,
+    content: string | null | undefined,
+}
+
+type ReadingSection = {
+    reading_passage: ReadingPassageDao | null | undefined,
+}
+
+type ReadingExamDao = {
+    reading_sections: ReadingSection | null | undefined,
 }
 
 export {
-    ReadingPassageDao ,
+    ReadingPassageDao,
+    ReadingSection,
+    ReadingExamDao,
 }
--- software-capstone-be/src/modules/readingSkill/services/reading_passage_service.ts
+++ software-capstone-be/src/modules/readingSkill/services/reading_passage_service.ts
@@ -1,48 +1,23 @@
-import {pool} from "../../../app/configs/database";
 import {ReadingPassageDao} from "../datatypes/exam";
+import {createReadingPassages as saveReadingPassages}  from "../daos/reading_passage_dao";
 
 function createReadingPassages(
         readingPassageDaos: ReadingPassageDao[]
     ): Promise<number[]> {
     
     return new Promise((resolve, reject) => {
-
-        const query: string = 
-        [
-            `INSERT INTO reading_passage (`,
-				`wallpaper`, `title`, `content`,
-			`)`, 
-            'VALUES ?',
-        ].join(' ');
-
-        const binderValues: (string|null)[][] = readingPassageDaos.map(dao => [
-            dao?.wallpaper ?? null, 
-            dao?.title ?? null, 
-            dao?.content ?? null
-        ]);
-
-        //User bulk insertion for better optimization
-        pool.query(query, [binderValues], (error, result) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-            
-            //Get the id of the created ones
-            const size: number = result.affectedRows;
-            const firstId: number = result.insertId;
-            const aboveMaxId: number = firstId + size;
-            let ids: number[] = [];
-            for (let i: number = firstId; i < aboveMaxId; i++) {
-                ids.push(i);
-            }
-            resolve(ids);
-        });
+        return saveReadingPassages(readingPassageDaos)
+            .catch(error => {
+                console.log(error);
+                reject(false);
+            })
+            .then((ids) => {
+                resolve(ids as number[]);
+            });
     })
     
 }
 
 export {
-    getReadingPassageByExamIds,
     createReadingPassages,
 }
