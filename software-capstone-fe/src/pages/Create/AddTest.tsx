579436fd6eccf8e856fef66d162d8e435db071ac
Ly Quoc Binh <53855738+TheHellhand@users.noreply.github.com>
2023-04-12 11:48:53 +0700

    Update AddTest.tsx

--- software-capstone-fe/src/pages/Create/AddTest.tsx
+++ software-capstone-fe/src/pages/Create/AddTest.tsx
@@ -3,11 +3,6 @@ import styles from "./AddTest.module.css";
 import { Row, Col, Tabs, Button } from "antd";
 import { AppHeader } from "../../components/AppHeader";
 import type { TabsProps } from "antd";
-// import SectionOne from "components/Section/SectionOne/SectionOne";
-// import AlphabetType from "components/QuestionType/MultipleChoice/AlphabetType";
-// import MappingHeadingInstruction from "components/Instruction/MappingHeadingInstruction/MappingHeadingInstruction";
-// import HeadingType from "components/QuestionType/MultipleChoice/HeadingType";
-// import AlphabetHeadingType from "components/QuestionType/MultipleChoice/AlphabetHeadingType";
 
 import ReadingParagraph from "components/ReadingParagraph";
 import QuestionSection from "components/QuestionSection";
@@ -19,23 +14,7 @@ import {
 } from "services/QuestionTypeService";
 import ReadingTestContext from "context/ReadingTestContext";
 
-const AddingTestPage = () => {
-  // const [letters, setLetters] = useState(
-  //   [
-  //     {value: "A", label: "A"},
-  //     {value: "B", label: "B"},
-  //     {value: "C", label: "C"},
-  //     {value: "D", label: "D"},
-  //     {value: "E", label: "E"},
-  //     {value: "F", label: "F"},
-  //     {value: "G", label: "G"},
-  //     {value: "H", label: "H"},
-  //   ]
-  // );
-
-  // const onChange = (key: string) => {
-  //   console.log(key);
-  
+const AddingTestPage = () => {  
   const [questionSectionKey, setQuestionSectionKey] = useState<number>(1);
   const [paragraphs, setParagraphs] = useState(generateReadingParagraphs);
   const [questionDetails, setQuestionDetails] = useState(
@@ -109,49 +88,6 @@ const AddingTestPage = () => {
             </Col>
           </Row>
         </Col>
-        {/* <Col className={`${styles.column} ${styles.right}`} span={12}>
-          <Row>
-            <Col span={24}>
-              <div className={styles.questionHeader} style={{ backgroundColor: 'var(--secondaryColor)' }}>Question 1 - 7</div>
-            </Col>
-          </Row>
-
-          <Row>
-            <TrueFalseInstruction from={1} to={7} />
-            <TrueFalseType order={1} />
-            <TrueFalseType order={2} />
-            <TrueFalseType order={3} />
-            <TrueFalseType order={4} />
-            <TrueFalseType order={5} />
-            <TrueFalseType order={6} />
-            <TrueFalseType order={7} />
-          </Row>
-
-          <Row>
-            <Col span={24}>
-              <div className={styles.questionHeader} style={{ backgroundColor: 'var(--secondaryColor)' }}>Question 27 - 33</div>
-            </Col>
-          </Row>
-
-          <Row>
-            <MappingHeadingInstruction letterFrom={"A"} letterTo={"H"} questionFrom={27} questionTo={33} />
-            <AlphabetHeadingType order={1} letterList={letters}/>
-            <AlphabetHeadingType order={2} letterList={letters}/>
-            <AlphabetHeadingType order={3} letterList={letters}/>
-            <AlphabetHeadingType order={4} letterList={letters}/>
-            <AlphabetHeadingType order={5} letterList={letters}/>
-            <AlphabetHeadingType order={6} letterList={letters}/>
-            <AlphabetHeadingType order={7} letterList={letters}/> 
-            <p style={{margin: "10px"}}>List of Headings</p> 
-            <HeadingType letter={"A"} />
-            <HeadingType letter={"B"} />
-            <HeadingType letter={"C"} />
-            <HeadingType letter={"D"} />
-            <HeadingType letter={"E"} />
-            <HeadingType letter={"F"} />
-            <HeadingType letter={"G"} />
-            <HeadingType letter={"H"} />
-          </Row> */}
 
         <Col
           className={`${styles.column} ${styles.right}`}
