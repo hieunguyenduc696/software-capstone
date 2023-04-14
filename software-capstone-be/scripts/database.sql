dd5281f6306550d750431819528ec403ea257173
Nhat Dang <nhatdang2604@gmail.com>
2023-04-14 21:27:04 +0700

    task/rename-entity-correctly: rename for synchronize the entity name, in db schema

--- software-capstone-be/scripts/database.sql
+++ software-capstone-be/scripts/database.sql
@@ -19,31 +19,31 @@ CREATE TABLE section (
     PRIMARY KEY(section_id)
 );
 
-CREATE TABLE task_type (
-    task_type_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE template_type (
+    template_type_id INT NOT NULL AUTO_INCREMENT,
     
     name VARCHAR(255) DEFAULT NULL,
     description LONGTEXT DEFAULT NULL,
 
-    PRIMARY KEY(task_type_id)
+    PRIMARY KEY( template_type_id)
 );
 
-CREATE TABLE task (
-    task_id INT NOT NULL AUTO_INCREMENT,
-    task_type_id INT DEFAULT NULL,
+CREATE TABLE template (
+    template_id INT NOT NULL AUTO_INCREMENT,
+    template_type_id INT DEFAULT NULL,
     section_id INT DEFAULT NULL,
 
-    task_index INT DEFAULT NULL,
+    template_index INT DEFAULT NULL,
     title TEXT DEFAULT NULL,
     content LONGTEXT DEFAULT NULL,
     expand_column LONGTEXT DEFAULT NULL,
 
-    PRIMARY KEY(task_id)
+    PRIMARY KEY(template_id)
 );
 
 CREATE TABLE question (
     question_id INT NOT NULL AUTO_INCREMENT,
-    task_id INT DEFAULT NULL,
+    template_id INT DEFAULT NULL,
 
     question_index INT DEFAULT NULL,
     content TEXT DEFAULT NULL,
@@ -56,17 +56,17 @@ CREATE TABLE answer (
     answer_id INT NOT NULL AUTO_INCREMENT,
     question_id INT DEFAULT NULL,
 
-    content TEXT DEFAULT NULL,
+    content LONGTEXT DEFAULT NULL,
     PRIMARY KEY(answer_id)
 );
 
 
-CREATE TABLE reading_passage (
-    reading_passage_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE paragraph (
+    paragraph_id INT NOT NULL AUTO_INCREMENT,
     section_id INT DEFAULT NULL,
     wallpaper VARCHAR(255) DEFAULT NULL,
     title TEXT DEFAULT NULL,
     content LONGTEXT DEFAULT NULL,
     
-    PRIMARY KEY(reading_passage_id)
+    PRIMARY KEY(paragraph_id)
 );
