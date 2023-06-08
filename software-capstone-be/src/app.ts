c131896038a08a4ef8970d565f5c07ae12ec4b2a
dangkl123 <nhatdang2604@gmail.com>
2023-06-08 23:58:13 +0700

    Add cors policy

--- software-capstone-be/src/app.ts
+++ software-capstone-be/src/app.ts
@@ -3,12 +3,14 @@
 import express, {Express} from 'express';
 import { APP_PORT } from './app/constants/config_constants';
 import * as readingRoutes from './modules/readingSkill/routes/route';
+import cors from 'cors';
 
 const app: Express = express();
 const port: string | undefined = APP_PORT;    //default port is 8088
 
 //Middlewares registrations
 app.use(express.json()); 
+app.use(cors());
 
 //Routes registrations
 app.use('/reading-skill/admin/test', readingRoutes.adminTest.router);
