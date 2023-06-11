ab033857d144c8f9a8c6f74641749003f30a0af4
dangkl123 <nhatdang2604@gmail.com>
2023-06-11 23:37:45 +0700

    Fix stuffs

--- software-capstone-be/src/app.ts
+++ software-capstone-be/src/app.ts
@@ -4,7 +4,6 @@ import express, {Express} from 'express';
 import cors from "cors";
 import { APP_PORT } from './app/constants/config_constants';
 import * as readingRoutes from './modules/readingSkill/routes/route';
-import cors from 'cors';
 
 const app: Express = express();
 const port: string | undefined = APP_PORT;    //default port is 8088
