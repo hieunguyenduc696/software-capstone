013d6b3fbc0290fdc1b4ae7d0ffe32396ef4c6a8
LUU HUYNH NGUYEN THI <luuhuynh.tkp@gmail.com>
2023-06-13 22:27:08 +0700

    update nav

--- software-capstone-fe/src/pages/user/home/components/Features/index.tsx
+++ software-capstone-fe/src/pages/user/home/components/Features/index.tsx
@@ -1,48 +1,59 @@
-import { Card, List, Typography } from 'antd'
-import React from 'react'
+import { Card, List, Typography } from "antd";
+import React from "react";
+import { useNavigate } from "react-router";
 
 function Features() {
-    const data = [
-        'https://i.pinimg.com/564x/aa/ea/12/aaea12e1c3a5a1ed35dbd87c3f15f45e.jpg',
-        'https://i.pinimg.com/564x/c0/1f/71/c01f719490b79c02ede76db8687e8a67.jpg',
-        'https://i.pinimg.com/564x/6f/2c/00/6f2c00585b1183995fd0690fff0ccc6b.jpg'
-    ]
+	const data = ["slide.png", "mock21.png", "mock22.jpg"];
+	const navigate = useNavigate();
 
-    return (
-        <>
-            <Typography.Title
-                level={2}
-                style={{
-                    margin: 0,
-                    textAlign: 'center',
-                    textTransform: 'uppercase'
-                }}>
-                Our Ielts Futures
-            </Typography.Title>
-            <List
-                style={{ marginTop: '1rem', paddingInline: '2rem' }}
-                grid={{
-                    gutter: 42,
-                    xs: 1,
-                    sm: 2,
-                    md: 2,
-                    lg: 3,
-                    xl: 3,
-                    xxl: 3
-                }}
-                dataSource={data}
-                renderItem={(item, index) => (
-                    <List.Item key={index}>
-                        <div>
-                            <img alt="" width={'100%'} height={320} src={`${item}`} style={{ objectFit: 'cover' }} />
-                            <Typography.Title level={3} style={{ textAlign: 'center', margin: 0 }}>Reading Tests</Typography.Title>
-                            <Typography.Text style={{ textAlign: 'center' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 15</Typography.Text>
-                        </div>
-                    </List.Item>
-                )}
-            />
-        </>
-    )
+	return (
+		<>
+			<Typography.Title
+				level={2}
+				style={{
+					margin: 0,
+					textAlign: "center",
+					textTransform: "uppercase",
+				}}
+			>
+				Our Ielts Futures
+			</Typography.Title>
+			<List
+				style={{ marginTop: "1rem", paddingInline: "2rem" }}
+				grid={{
+					gutter: 42,
+					xs: 1,
+					sm: 2,
+					md: 2,
+					lg: 3,
+					xl: 3,
+					xxl: 3,
+				}}
+				dataSource={data}
+				renderItem={(item, index) => (
+					<List.Item key={index} style={{ cursor: "pointer" }}>
+						<div>
+							<img
+								alt=""
+								width={"100%"}
+								height={320}
+								src={`${item}`}
+								style={{ objectFit: "cover" }}
+							/>
+							<Typography.Title level={3} style={{ textAlign: "center", margin: 0 }}>
+								Reading Tests
+							</Typography.Title>
+							<Typography.Text style={{ textAlign: "center" }}>
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry. Lorem Ipsum has been the industry's standard dummy text
+								ever since the 15
+							</Typography.Text>
+						</div>
+					</List.Item>
+				)}
+			/>
+		</>
+	);
 }
 
-export default Features
+export default Features;
--- software-capstone-fe/src/pages/user/home/components/Items/index.tsx
+++ software-capstone-fe/src/pages/user/home/components/Items/index.tsx
@@ -1,57 +1,59 @@
-import React from 'react';
-import { Button, List, Typography } from 'antd';
-import styles from './Items.module.css'
-import { RightOutlined } from '@ant-design/icons';
+import React from "react";
+import { Button, List, Typography } from "antd";
+import styles from "./Items.module.css";
+import { RightOutlined } from "@ant-design/icons";
 
-const data = [
-    'slide.png',
-    'https://i.pinimg.com/564x/aa/ea/12/aaea12e1c3a5a1ed35dbd87c3f15f45e.jpg',
-    'https://i.pinimg.com/564x/c0/1f/71/c01f719490b79c02ede76db8687e8a67.jpg',
-    'https://i.pinimg.com/564x/6f/2c/00/6f2c00585b1183995fd0690fff0ccc6b.jpg'
-];
+const data = ["slide.png", "mock.png", "mock21.png", "mock22.jpg"];
 
 const Items: React.FC = () => (
-    <>
-        <Typography.Title
-            level={2}
-            style={{
-                margin: 0,
-                textAlign: 'center',
-                textTransform: 'uppercase'
-            }}>
-            Newest Ielts Tests
-        </Typography.Title>
-        <List
-            style={{ marginTop: '1rem' }}
-            grid={{
-                gutter: 32,
-                xs: 1,
-                sm: 2,
-                md: 2,
-                lg: 4,
-                xl: 4,
-                xxl: 4
-            }}
-            dataSource={data}
-            renderItem={(item) => (
-                <List.Item>
-                    <img className={styles['responsive-image']} alt='' src={item} style={{ objectFit: 'cover', borderRadius: '.5rem' }} />
-                </List.Item>
-            )}
-        />
-        <Button
-            type='text'
-            className={styles['btn-see-more']}
-            style={{
-                textTransform: 'uppercase',
-                width: '100%',
-                textAlign: 'center',
-                display: 'flex',
-                justifyContent: 'center',
-            }}>
-            See More <RightOutlined />
-        </Button>
-    </>
+	<>
+		<Typography.Title
+			level={2}
+			style={{
+				margin: 0,
+				textAlign: "center",
+				textTransform: "uppercase",
+			}}
+		>
+			Newest Ielts Tests
+		</Typography.Title>
+		<List
+			style={{ marginTop: "1rem" }}
+			grid={{
+				gutter: 32,
+				xs: 1,
+				sm: 2,
+				md: 2,
+				lg: 4,
+				xl: 4,
+				xxl: 4,
+			}}
+			dataSource={data}
+			renderItem={(item) => (
+				<List.Item>
+					<img
+						className={styles["responsive-image"]}
+						alt=""
+						src={item}
+						style={{ objectFit: "cover", borderRadius: ".5rem" }}
+					/>
+				</List.Item>
+			)}
+		/>
+		<Button
+			type="text"
+			className={styles["btn-see-more"]}
+			style={{
+				textTransform: "uppercase",
+				width: "100%",
+				textAlign: "center",
+				display: "flex",
+				justifyContent: "center",
+			}}
+		>
+			See More <RightOutlined />
+		</Button>
+	</>
 );
 
 export default Items;
